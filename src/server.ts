cb41fda7e82d0a92a8f08f3d7275d35631717704
User64 <user64@idreambiz.com>
2022-12-23 00:28:47 +0530

    Added documentation for server initialization in server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { app, init } from './app'
 
+/** Reads and stored port number from environment with 3000 as default. */
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    init()
-})
+/** Starts app on specified port and bootstraps the application using init. */
+app.listen(PORT, () => { init() })
